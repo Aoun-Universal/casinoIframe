3993582c23f4ce424f5dfd75c131974ac8143797
Sidra Khalid <147918296+sidrakhalid4488@users.noreply.github.com>
2024-11-05 15:45:21 +0500

    home page added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,12 @@ export const routes: Routes = [
     {
         path: '',
         loadComponent: () => import('./components/layout/layout.component').then(c => c.LayoutComponent),
-        // children:[
-        //     {}
-        // ]
+        children:[
+            {
+                path:'',
+        loadComponent: () => import('./pages/home/home.component').then(c => c.HomeComponent),
+
+            }
+        ]
     }
 ];
