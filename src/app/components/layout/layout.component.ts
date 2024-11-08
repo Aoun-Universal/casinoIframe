2bb0bb5cb6730896887b1b040a003baeb51b4722
waseemahmad <waseemahmedofficial2000@gmail.com>
2024-11-08 18:25:12 +0500

    bottom navogation done

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,28 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { ConditionHandlerService } from '../../services/condition-handler.service';
-import { NavigationEnd, Router } from '@angular/router';
-import { ContentComponent } from '../content/content.component';
-import { SidebarComponent } from '../../shared/sidebar/sidebar.component';
-import { FooterComponent } from '../../shared/footer/footer.component';
-import { CommonModule } from '@angular/common';
+import {Component} from '@angular/core';
+import {NavigationEnd, Router} from '@angular/router';
+import {ContentComponent} from '../content/content.component';
+import {SidebarComponent} from '../../shared/sidebar/sidebar.component';
+import {FooterComponent} from '../../shared/footer/footer.component';
+import {CommonModule} from '@angular/common';
 import {HeaderComponent} from "../../shared/header/header.component";
-import { filter, map, Observable } from 'rxjs';
+import {filter} from 'rxjs';
+import {MobileNavigationComponent} from "../../shared/mobile-navigation/mobile-navigation.component";
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [ContentComponent, SidebarComponent, FooterComponent, CommonModule, HeaderComponent],
+  imports: [ContentComponent, SidebarComponent, FooterComponent, CommonModule, HeaderComponent, MobileNavigationComponent],
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.css'] 
+  styleUrls: ['./layout.component.css']
 })
-export class LayoutComponent  {
-  currentRoute: string = '';  
+export class LayoutComponent {
+  currentRoute: string = '';
+
   constructor(private router: Router) {
     this.router.events.pipe(
       filter(event => event instanceof NavigationEnd)
     ).subscribe((event: NavigationEnd) => {
       this.currentRoute = event.urlAfterRedirects;
-      console.log(this.currentRoute); 
+      console.log(this.currentRoute);
     });
   }
 
