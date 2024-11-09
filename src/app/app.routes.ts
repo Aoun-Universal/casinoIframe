b02b896082e42edb0a9f3fb565e76145be142aad
abrandhawa <alibakhat01@gmail.com>
2024-11-09 13:05:19 +0500

    created route for soccer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,37 +4,58 @@ import { RegisterComponent } from './modal/register/register.component';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./components/layout/layout.component').then(c => c.LayoutComponent),
+    loadComponent: () =>
+      import('./components/layout/layout.component').then(
+        (c) => c.LayoutComponent
+      ),
     children: [
       {
         path: '',
-        loadComponent:()=> import ('./pages/before-login/before-login.component').then(c => c.BeforeLoginComponent),
+        loadComponent: () =>
+          import('./pages/before-login/before-login.component').then(
+            (c) => c.BeforeLoginComponent
+          ),
       },
       {
         path: 'home',
-        loadComponent: () => import('./pages/home/home.component').then(c => c.HomeComponent),
+        loadComponent: () =>
+          import('./pages/home/home.component').then((c) => c.HomeComponent),
       },
       {
         path: 'drake',
-        loadComponent: () => import('./pages/drake/drake.component').then(c => c.DrakeComponent),
+        loadComponent: () =>
+          import('./pages/drake/drake.component').then((c) => c.DrakeComponent),
       },
 
       {
         path: 'responsible-gambling',
-        loadComponent: () => import('./pages/responsible-gambling/responsible-gambling.component').then(c => c.ResponsibleGamblingComponent),
+        loadComponent: () =>
+          import(
+            './pages/responsible-gambling/responsible-gambling.component'
+          ).then((c) => c.ResponsibleGamblingComponent),
       },
       {
-        path:'sport-market',
-        loadComponent: () => import('./pages/sports-markets/sports-markets.component').then(c=>c.SportsMarketsComponent)
-      }
-    ]
+        path: 'sport-market',
+        loadComponent: () =>
+          import('./pages/sports-markets/sports-markets.component').then(
+            (c) => c.SportsMarketsComponent
+          ),
+      },
+      {
+        path: 'soccer',
+        loadComponent: () =>
+          import('./pages/soccer/soccer.component').then(
+            (c) => c.SoccerComponent
+          ),
+      },
+    ],
   },
   {
     path: 'login',
-    component: RegisterComponent
+    component: RegisterComponent,
   },
   {
     path: '**',
     redirectTo: '',
-  }
+  },
 ];
