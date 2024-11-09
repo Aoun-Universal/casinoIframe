525444a1b06e1aa15af8f4f3dbbec26c5d6b6e22
ARSLAN-choudary <139113663+ARSLAN-choudary@users.noreply.github.com>
2024-11-09 17:59:21 +0500

    settings is pending

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { VaultComponent } from './modal/vault/vault.component';
 import { VipComponent } from './modal/vip/vip.component';
+import { StatisticsComponent } from './modal/statistics/statistics.component';
+import { NotificationComponent } from './modal/notification/notification.component';
+import { SettingsComponent } from './modal/settings-modals/settings/settings.component';
 
 export const routes: Routes = [
   {
@@ -24,6 +27,18 @@ export const routes: Routes = [
   {
     path:'vip',
     component:VipComponent
+  },
+  {
+    path:'statistics',
+    component:StatisticsComponent
+  },
+  {
+    path:'notification',
+    component:NotificationComponent
+  },
+  {
+    path:'setting',
+    component:SettingsComponent
   },
   {
     path: '**',
