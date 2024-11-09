7e8c4870670aa835a56ddbd629a2a4ce269f0bf4
faizan99904 <faizan99904@gmail.com>
2024-11-09 14:02:14 +0500

    add condition for route

--- src/app/pages/responsible-gambling/responsible-gambling.component.ts
+++ src/app/pages/responsible-gambling/responsible-gambling.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { StakeSmartComponent } from '../stake-smart/stake-smart.component';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-responsible-gambling',
@@ -9,5 +11,17 @@ import { StakeSmartComponent } from '../stake-smart/stake-smart.component';
   styleUrl: './responsible-gambling.component.css'
 })
 export class ResponsibleGamblingComponent {
+  routerPath: any
+  constructor(private router: Router, ) { }
+  ngOnInit(): void {
+    this.router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd)
+      )
+      .subscribe((event: NavigationEnd) => {
+        this.routerPath = event.url;
 
+      });
+    this.routerPath = this.router.url;
+  }
 }
