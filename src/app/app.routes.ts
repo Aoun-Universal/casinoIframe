ef1e99f093dd952032ad528fa38ead360c82f7e2
waseemahmad <waseemahmedofficial2000@gmail.com>
2024-11-09 15:23:37 +0500

    changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
-import { Routes } from '@angular/router';
-import { RegisterComponent } from './modal/register/register.component';
-import { StakeSmartComponent } from './pages/stake-smart/stake-smart.component';
+import {Routes} from '@angular/router';
+import {RegisterComponent} from './modal/register/register.component';
+import {StakeSmartComponent} from './pages/stake-smart/stake-smart.component';
+import {LogoutComponent} from './modal/logout/logout.component';
 
 export const routes: Routes = [
   {
@@ -9,7 +10,7 @@ export const routes: Routes = [
     children: [
       {
         path: '',
-        loadComponent:()=> import ('./pages/before-login/before-login.component').then(c => c.BeforeLoginComponent),
+        loadComponent: () => import ('./pages/before-login/before-login.component').then(c => c.BeforeLoginComponent),
       },
       {
         path: 'home',
@@ -23,16 +24,16 @@ export const routes: Routes = [
       {
         path: 'responsible-gambling',
         loadComponent: () => import('./pages/responsible-gambling/responsible-gambling.component').then(c => c.ResponsibleGamblingComponent),
-        children:[
+        children: [
           {
-            path:'stake-smart',
-            component:StakeSmartComponent
+            path: 'stake-smart',
+            component: StakeSmartComponent
           }
         ]
       },
       {
-        path:'sport-market',
-        loadComponent: () => import('./pages/sports-markets/sports-markets.component').then(c=>c.SportsMarketsComponent)
+        path: 'sport-market',
+        loadComponent: () => import('./pages/sports-markets/sports-markets.component').then(c => c.SportsMarketsComponent)
       }
     ]
   },
