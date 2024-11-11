2fbae2652d230a1d0c78d1cd8d3f9ea620b3b63f
abrandhawa <alibakhat01@gmail.com>
2024-11-11 14:47:02 +0500

    uptop tab view completed

--- src/app/pages/soccer/soccer.component.ts
+++ src/app/pages/soccer/soccer.component.ts
@@ -10,15 +10,21 @@ import { Component } from '@angular/core';
 })
 export class SoccerComponent {
   activeTab: string = 'live&Upcoming';
+  activeTab2: string = 'allBets';
   standardView: boolean = true;
   catDropdownState: boolean = false;
   catDropdownSelectedVal: string = 'Winner';
   eventDropdownState: boolean = true;
   subEventDropdown: boolean = true;
   isLive: boolean = true;
+  soccerBettingOddsState: boolean = false;
 
   setActiveTab(val: string) {
     this.activeTab = val;
+  }
+
+  setActiveTab2(val: string) {
+    this.activeTab2 = val;
   }
 
   toggleStandardView() {
@@ -41,5 +47,8 @@ export class SoccerComponent {
 
   toggleSubEventDropdown() {
     this.subEventDropdown = !this.subEventDropdown;
+  }
+  toggleSoccerBettingOddState() {
+    this.soccerBettingOddsState = !this.soccerBettingOddsState;
   }
 }
