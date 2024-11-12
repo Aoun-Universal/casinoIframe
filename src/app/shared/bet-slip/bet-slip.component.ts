98459d5e4702371401971662a39bed671de37c1e
faizan99904 <faizan99904@gmail.com>
2024-11-12 11:26:20 +0500

    betSlip toggle done

--- src/app/shared/bet-slip/bet-slip.component.ts
+++ src/app/shared/bet-slip/bet-slip.component.ts
@@ -1,45 +1,45 @@
 import { NgClass, NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ToggleService } from '../../services/toggle.service';
 
 @Component({
   selector: 'app-bet-slip',
   standalone: true,
-  imports: [NgIf,NgClass],
+  imports: [NgIf, NgClass],
   templateUrl: './bet-slip.component.html',
   styleUrl: './bet-slip.component.css'
 })
-export class BetSlipComponent {
-  WinnerDropdown=false;
+export class BetSlipComponent implements OnInit {
+  WinnerDropdown = false;
   activeTab: number = 1;
+  betSlip = false
 
   toggleDropdown() {
     this.WinnerDropdown = !this.WinnerDropdown;
   }
 
   // Odds dropdown
-  OddsDropdown=false;
+  OddsDropdown = false;
   toggleOddsDropdown() {
     this.OddsDropdown = !this.OddsDropdown;
   }
 
-   //  Switch Tabs
-   setActiveTab(tabIndex: number): void {
+  //  Switch Tabs
+  setActiveTab(tabIndex: number): void {
     this.activeTab = tabIndex;
   }
 
   // Close Betslip
   showModal: boolean = false;
-  constructor(private toggleService: ToggleService) {}
+  constructor(private toggleService: ToggleService) { }
   ngOnInit(): void {
     this.toggleService.getBetslipState().subscribe(value => {
-      this.showModal = value;
+      this.betSlip = value;
     });
   }
-  
+
   closeModal() {
-    this.toggleService.setBetslipstate(false);
+    this.toggleService.setBetslipstate(!this.betSlip);
   }
 
-  
 }
