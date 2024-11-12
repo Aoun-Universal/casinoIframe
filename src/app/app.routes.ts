3cb9fa0ea583f9f523d5acf442043c05be12b954
faizan99904 <faizan99904@gmail.com>
2024-11-12 17:36:27 +0500

    working on route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -43,7 +43,7 @@ export const routes: Routes = [
         component:MyBetsComponent,
       },
        {
-        path: 'Affiliate',
+        path: 'affiliate',
         component: AffiliateComponent,
       },
       {
