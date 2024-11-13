5d61bc6f60a8a48e926f616c50b1a4a048cc793e
faizan99904 <faizan99904@gmail.com>
2024-11-13 15:52:38 +0500

    add tabs in horse racing

--- src/app/pages/horse-racing/horse-racing.component.ts
+++ src/app/pages/horse-racing/horse-racing.component.ts
@@ -1,29 +1,30 @@
 import { CommonModule } from '@angular/common';
 import { Component, CUSTOM_ELEMENTS_SCHEMA, HostListener, ViewChild } from '@angular/core';
 import { SlickCarouselComponent, SlickCarouselModule } from 'ngx-slick-carousel';
+import { TabSliderComponent } from '../../shared/tab-slider/tab-slider.component';
 @Component({
   selector: 'app-horse-racing',
   standalone: true,
-  imports: [CommonModule, SlickCarouselModule],
+  imports: [CommonModule, SlickCarouselModule, TabSliderComponent],
   templateUrl: './horse-racing.component.html',
   styleUrl: './horse-racing.component.css',
-  schemas:[CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HorseRacingComponent {
 
-@ViewChild('slickModal') slickModal!: SlickCarouselComponent;
-  tabKey:string='friday'
-  accIsOpen:boolean=true
-  marketType='Horse Racing';
-  marketSelection:boolean=false;
+  @ViewChild('slickModal') slickModal!: SlickCarouselComponent;
+  tabKey: string = 'friday'
+  accIsOpen: boolean = true
+  marketType = 'Horse Racing';
+  marketSelection: boolean = false;
   currentSlideIndex = 0;
   slideCount = 0;
 
-   racingMarketConfig = {
+  racingMarketConfig = {
     slidesToShow: 3,
     slidesToScroll: 3,
-    arrows:false,
-    infinite:false,
+    arrows: false,
+    infinite: false,
     responsive: [
       {
         breakpoint: 1235,
@@ -52,36 +53,36 @@ export class HorseRacingComponent {
 
   prev() {
 
-    if(this.currentSlideIndex!==0){
+    if (this.currentSlideIndex !== 0) {
       this.slickModal.slickPrev();
     }
   }
 
   next() {
 
-    if(this.currentSlideIndex!==this.slideCount){
+    if (this.currentSlideIndex !== this.slideCount) {
       this.slickModal.slickNext();
     }
   }
 
-  carousel=[1,2,3,4,5,6,7,8]
+  carousel = [1, 2, 3, 4, 5, 6, 7, 8]
 
-  onTabClick(key:string){
-    if(key){
-      this.tabKey=key
+  onTabClick(key: string) {
+    if (key) {
+      this.tabKey = key
     }
   }
-  onAllRaceAccClick(){
-    this.accIsOpen=!this.accIsOpen
+  onAllRaceAccClick() {
+    this.accIsOpen = !this.accIsOpen
   }
-  toggleMarketType(){
-    this.marketSelection=!this.marketSelection
+  toggleMarketType() {
+    this.marketSelection = !this.marketSelection
   }
-  onMarketTypeSelect(key:string){
-    if(key){
-      this.marketType=key
+  onMarketTypeSelect(key: string) {
+    if (key) {
+      this.marketType = key
     }
-    this.marketSelection=!this.marketSelection
+    this.marketSelection = !this.marketSelection
   }
 
   @HostListener("document:click", ["$event"])
@@ -90,7 +91,25 @@ export class HorseRacingComponent {
     const clickedInside = targetElement.closest(".market-selection-container");
 
     if (!clickedInside) {
-      this.marketSelection=false
+      this.marketSelection = false
     }
   }
+
+  tabsItem = [
+    { tabTitle: 'Friday 08', dotState: false },
+    { tabTitle: 'Saturday 09', dotState: false },
+    { tabTitle: 'Yesterday 10', dotState: false },
+    { tabTitle: 'Today 11', dotState: false },
+    { tabTitle: 'Tommorrow 12', dotState: false },
+    { tabTitle: 'Wednesday 13', dotState: false },
+    { tabTitle: ' Thursday 14', dotState: false },
+  ]
+
+  tabsItems = [
+    { tabTitle: 'All Bets', dotState: false },
+    { tabTitle: 'High Rollers', dotState: false },
+    { tabTitle: 'Race Leaderboard', dotState: false },
+  ]
+
+  
 }
