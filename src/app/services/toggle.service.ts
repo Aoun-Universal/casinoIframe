fd8f3fcfa0e9e82dd2a4f094e03064551eacd294
ARSLAN-choudary <139113663+ARSLAN-choudary@users.noreply.github.com>
2024-11-13 15:01:02 +0500

    syncing

--- src/app/services/toggle.service.ts
+++ src/app/services/toggle.service.ts
@@ -3,33 +3,47 @@ import { Inject, Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ToggleService {
-  private sidebarState: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
-  private Betslip: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  private sidebarState: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(
+    true
+  );
+  private modalState: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false); // Modal initially hidden
+  modalState$ = this.modalState.asObservable();
 
-  sidebarState$=this.sidebarState.asObservable()
-  constructor(
-  ) { 
-   
-  }
-  getSidebar(){
+  sidebarState$ = this.sidebarState.asObservable();
+  constructor() {}
+
+  getSidebar() {
     return this.sidebarState;
   }
-  setSidebar(value:boolean){
-    this.sidebarState.next(value)
+  setSidebar(value: boolean) {
+    this.sidebarState.next(value);
   }
-  toggleSidebar(){
-    this.sidebarState.next(!this.sidebarState.value)
+  toggleSidebar() {
+    this.sidebarState.next(!this.sidebarState.value);
+  }
+
+  openModal() {
+    this.modalState.next(true); 
+  }
+
+  closeModal() {
+    this.modalState.next(false);
+  }
+
+  toggleModal() {
+    this.modalState.next(!this.modalState.value);
   }
 
   // Betslip
    // Loyalty Terms Modal
-   getBetslipState() {
-    return this.Betslip;
-  }
-  setBetslipstate(value: boolean) {
-    this.Betslip.next(value);
-  }
+  //  getBetslipState() {
+  //   return this.Betslip;
+  // }
+  // setBetslipstate(value: boolean) {
+  //   this.Betslip.next(value);
+  // }
 }
+  
