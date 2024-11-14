178d04b3569fc2706a78f0de91aec423c693f6c9
hinaasghar005 <hinaasghar005@gmail.com>
2024-11-14 12:50:59 +0500

    Added Dropdown+ Arrow Toggle+ section

--- src/app/pages/racing-market-details/racing-market-details.component.ts
+++ src/app/pages/racing-market-details/racing-market-details.component.ts
@@ -12,8 +12,27 @@ export class RacingMarketDetailsComponent {
   activeTab: number = 1;
   isFluctuationVisible = false; // Initially hidden
   isMarketOpen = true;
+  isMarketDetailOpen = true;
+  WinnerDropdown = false;
+  isTabsData = false;
+  isArrowUpSelected: boolean = true; // Default to up arrow
+  isArrowWinSelected: boolean = true; 
+  toggleArrow() {
+    this.isArrowUpSelected = !this.isArrowUpSelected; 
+  }
+  toggleArrowWin() {
+    this.isArrowWinSelected = !this.isArrowWinSelected;
+  }
+
+  
+  toggleDropdown() {
+    this.WinnerDropdown = !this.WinnerDropdown;
+  }
 
 
+  toggleTabs() {
+    this.isTabsData = !this.isTabsData;
+  }
   toggleVisibility() {
     this.isFluctuationVisible = !this.isFluctuationVisible;
   }
@@ -26,4 +45,9 @@ export class RacingMarketDetailsComponent {
   toggleMarket() {
     this.isMarketOpen = !this.isMarketOpen;
   }
+   // Market 2
+   toggleMarketDetail() {
+    this.isMarketDetailOpen = !this.isMarketDetailOpen;
+  }
+  
 }
