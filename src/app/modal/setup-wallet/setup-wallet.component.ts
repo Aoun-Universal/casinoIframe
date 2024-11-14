f9c0c7e299c47634f9e207a7d4baa2093dacff57
faizan99904 <faizan99904@gmail.com>
2024-11-14 23:13:26 +0500

    working on modal

--- src/app/modal/setup-wallet/setup-wallet.component.ts
+++ src/app/modal/setup-wallet/setup-wallet.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-setup-wallet',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './setup-wallet.component.html',
   styleUrl: './setup-wallet.component.css'
 })
 export class SetupWalletComponent {
+    stepCount = 1
+    counter(){
+      if(this.stepCount < 2){
+        this.stepCount++ 
+      }
+    }
 
 }
