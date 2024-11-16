9a8a9ccad666bb91fdeb7e2a32cc5f1ea4539880
faizan99904 <faizan99904@gmail.com>
2024-11-16 12:49:30 +0500

    add ngx toastr

--- src/app/pages/my-bets/my-bets.component.ts
+++ src/app/pages/my-bets/my-bets.component.ts
@@ -1,28 +1,39 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { TabSliderComponent } from "../../shared/tab-slider/tab-slider.component";
+import { ToastrModule, ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-my-bets',
   standalone: true,
-  imports: [CommonModule, TabSliderComponent],
+  imports: [CommonModule, TabSliderComponent, ToastrModule],
   templateUrl: './my-bets.component.html',
   styleUrl: './my-bets.component.css'
 })
 export class MyBetsComponent {
-  activeTb:string='casino';
-  openModal:boolean=false
+  activeTb: string = 'casino';
+  openModal: boolean = false
   activeTab: number = 1;
-  LiveTab = 'basketball'; 
+  LiveTab = 'basketball';
   TableTab: number = 1;
-  dropdown:boolean=false
-  tabDropdown:string='active'
-  openDropdown(tab:string){
-    this.tabDropdown=tab
-    this.dropdown=!this.dropdown
+  dropdown: boolean = false
+  tabDropdown: string = 'active'
+
+  constructor(private toastr: ToastrService) { }
+
+  showSuccess() {
+    this.toastr.success('This is a success message!', 'Title', {
+      positionClass: 'custom-toast-top-left',
+      progressBar:true
+    });
   }
-  openmenuModal():void{
-    this.openModal=!this.openModal
+
+  openDropdown(tab: string) {
+    this.tabDropdown = tab
+    this.dropdown = !this.dropdown
+  }
+  openmenuModal(): void {
+    this.openModal = !this.openModal
   }
   setActivTab(tab: string): void {
     this.activeTb = tab; // Update active tab on click
@@ -32,13 +43,13 @@ export class MyBetsComponent {
     this.activeTab = tabIndex;
   }
 
-    // Tabs
-    setActiveTableTab(tabIndex: number): void {
-      this.TableTab = tabIndex;
-    }
+  // Tabs
+  setActiveTableTab(tabIndex: number): void {
+    this.TableTab = tabIndex;
+  }
 
-   // Table Tabs
-   setLiveTabActive(tab: string) {
+  // Table Tabs
+  setLiveTabActive(tab: string) {
     this.LiveTab = tab;
   }
 
@@ -52,6 +63,6 @@ export class MyBetsComponent {
   tabsItems = [
     { tabTitle: 'Casino', dotState: false },
     { tabTitle: 'Sports', dotState: false },
-   
+
   ]
 }
