662e5d18552e0b1e9913ae48e6958905df52d2ec
abrandhawa <alibakhat01@gmail.com>
2024-12-31 17:32:49 +0500

    changes

--- src/app/shared/mob-navigation/mob-navigation.component.ts
+++ src/app/shared/mob-navigation/mob-navigation.component.ts
@@ -6,28 +6,48 @@ import { ToggleService } from '../../services/toggle.service';
   standalone: true,
   imports: [],
   templateUrl: './mob-navigation.component.html',
-  styleUrl: './mob-navigation.component.css'
+  styleUrl: './mob-navigation.component.css',
 })
 export class MobNavigationComponent implements OnInit {
   mobSideBarState: boolean = false;
+  viewType = 'Profile';
+
   constructor(private toggle: ToggleService) {}
   ngOnInit(): void {
     this.getMobSideBarState();
+    this.getViewType();
   }
 
+  getViewType() {
+    this.toggle.getMobSideBarContent().subscribe((val: string) => {
+      this.viewType = val;
+    });
+  }
   getMobSideBarState() {
     this.toggle.getMobileSideBarState().subscribe((val: boolean) => {
       this.mobSideBarState = val;
     });
   }
   openBrowseMobSidebar() {
-    this.toggle.setMobSideBarState(!this.mobSideBarState);
-    this.toggle.setMobSideBarContent('Game');
+    if (this.mobSideBarState && this.viewType === 'Profile') {
+      this.toggle.setMobSideBarContent('Game');
+    } else if (this.mobSideBarState && this.viewType === 'Game') {
+      this.closeMobSideBar();
+    } else {
+      this.toggle.setMobSideBarState(!this.mobSideBarState);
+      this.toggle.setMobSideBarContent('Game');
+    }
   }
 
   openProfileMobSidebar() {
-    this.toggle.setMobSideBarState(!this.mobSideBarState);
-    this.toggle.setMobSideBarContent('Profile');
+    if (this.mobSideBarState && this.viewType === 'Game') {
+      this.toggle.setMobSideBarContent('Profile');
+    } else if (this.mobSideBarState && this.viewType === 'Profile') {
+      this.closeMobSideBar();
+    } else {
+      this.toggle.setMobSideBarState(!this.mobSideBarState);
+      this.toggle.setMobSideBarContent('Profile');
+    }
   }
   closeMobSideBar() {
     this.toggle.setMobSideBarState(false);
